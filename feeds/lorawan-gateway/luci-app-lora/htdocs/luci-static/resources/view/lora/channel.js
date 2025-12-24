bcb0ca157edf38d42df20267c01fe7ad40e060c8
qian <ruiqian.tang@seeed.cc>
2025-12-24 17:04:13 +0800

    feat: Update the RF power logic configuration of the BasicStation gateway.

--- feeds/lorawan-gateway/luci-app-lora/htdocs/luci-static/resources/view/lora/channel.js
+++ feeds/lorawan-gateway/luci-app-lora/htdocs/luci-static/resources/view/lora/channel.js
@@ -3,142 +3,6 @@
 'require form';
 'require uci';
 
-const tx_gain_lut_915_low = [
-    { rf_power: 1, pa_gain: 0, pwr_idx: 5 },
-    { rf_power: 2, pa_gain: 0, pwr_idx: 6 },
-    { rf_power: 3, pa_gain: 0, pwr_idx: 7 },
-    { rf_power: 4, pa_gain: 0, pwr_idx: 8 },
-    { rf_power: 5, pa_gain: 0, pwr_idx: 9 },
-    { rf_power: 6, pa_gain: 0, pwr_idx: 10 },
-    { rf_power: 7, pa_gain: 0, pwr_idx: 11 },
-    { rf_power: 8, pa_gain: 0, pwr_idx: 12 },
-    { rf_power: 9, pa_gain: 0, pwr_idx: 13 },
-    { rf_power: 10, pa_gain: 0, pwr_idx: 14 },
-    { rf_power: 11, pa_gain: 0, pwr_idx: 15 },
-    { rf_power: 12, pa_gain: 0, pwr_idx: 16 },
-    { rf_power: 13, pa_gain: 0, pwr_idx: 17 },
-    { rf_power: 14, pa_gain: 0, pwr_idx: 18 },
-    { rf_power: 15, pa_gain: 0, pwr_idx: 19 },
-    { rf_power: 16, pa_gain: 1, pwr_idx: 3 },
-    { rf_power: 17, pa_gain: 1, pwr_idx: 4 },
-    { rf_power: 18, pa_gain: 1, pwr_idx: 5 },
-    { rf_power: 19, pa_gain: 1, pwr_idx: 6 },
-    { rf_power: 20, pa_gain: 1, pwr_idx: 7 },
-    { rf_power: 21, pa_gain: 1, pwr_idx: 8 },
-    { rf_power: 22, pa_gain: 1, pwr_idx: 9 },
-    { rf_power: 23, pa_gain: 1, pwr_idx: 11 },
-    { rf_power: 24, pa_gain: 1, pwr_idx: 12 },
-    { rf_power: 25, pa_gain: 1, pwr_idx: 13 },
-    { rf_power: 26, pa_gain: 1, pwr_idx: 14 },
-    { rf_power: 27, pa_gain: 1, pwr_idx: 15 }
-];
-const tx_gain_lut_915_high = [
-    { rf_power: 1, pa_gain: 0, pwr_idx: 5 },
-    { rf_power: 2, pa_gain: 0, pwr_idx: 6 },
-    { rf_power: 3, pa_gain: 0, pwr_idx: 7 },
-    { rf_power: 4, pa_gain: 0, pwr_idx: 8 },
-    { rf_power: 5, pa_gain: 0, pwr_idx: 9 },
-    { rf_power: 6, pa_gain: 0, pwr_idx: 10 },
-    { rf_power: 7, pa_gain: 0, pwr_idx: 11 },
-    { rf_power: 8, pa_gain: 0, pwr_idx: 12 },
-    { rf_power: 9, pa_gain: 0, pwr_idx: 13 },
-    { rf_power: 10, pa_gain: 0, pwr_idx: 14 },
-    { rf_power: 11, pa_gain: 0, pwr_idx: 15 },
-    { rf_power: 12, pa_gain: 0, pwr_idx: 16 },
-    { rf_power: 13, pa_gain: 0, pwr_idx: 17 },
-    { rf_power: 14, pa_gain: 0, pwr_idx: 18 },
-    { rf_power: 15, pa_gain: 0, pwr_idx: 19 },
-    { rf_power: 16, pa_gain: 1, pwr_idx: 3 },
-    { rf_power: 17, pa_gain: 1, pwr_idx: 4 },
-    { rf_power: 18, pa_gain: 1, pwr_idx: 5 },
-    { rf_power: 19, pa_gain: 1, pwr_idx: 6 },
-    { rf_power: 20, pa_gain: 1, pwr_idx: 7 },
-    { rf_power: 21, pa_gain: 1, pwr_idx: 8 },
-    { rf_power: 22, pa_gain: 1, pwr_idx: 9 },
-    { rf_power: 23, pa_gain: 1, pwr_idx: 11 },
-    { rf_power: 24, pa_gain: 1, pwr_idx: 12 },
-    { rf_power: 25, pa_gain: 1, pwr_idx: 13 },
-    { rf_power: 26, pa_gain: 1, pwr_idx: 14 },
-    { rf_power: 27, pa_gain: 1, pwr_idx: 15 }
-];
-const tx_gain_lut_868_low = [
-    { rf_power: 1, pa_gain: 0, pwr_idx: 4 },
-    { rf_power: 2, pa_gain: 0, pwr_idx: 5 },
-    { rf_power: 3, pa_gain: 0, pwr_idx: 6 },
-    { rf_power: 4, pa_gain: 0, pwr_idx: 7 },
-    { rf_power: 5, pa_gain: 0, pwr_idx: 8 },
-    { rf_power: 6, pa_gain: 0, pwr_idx: 9 },
-    { rf_power: 7, pa_gain: 0, pwr_idx: 10 },
-    { rf_power: 8, pa_gain: 0, pwr_idx: 11 },
-    { rf_power: 9, pa_gain: 0, pwr_idx: 12 },
-    { rf_power: 10, pa_gain: 0, pwr_idx: 13 },
-    { rf_power: 11, pa_gain: 0, pwr_idx: 14 },
-    { rf_power: 12, pa_gain: 0, pwr_idx: 15 },
-    { rf_power: 13, pa_gain: 0, pwr_idx: 16 },
-    { rf_power: 14, pa_gain: 0, pwr_idx: 17 },
-    { rf_power: 15, pa_gain: 0, pwr_idx: 18 },
-    { rf_power: 16, pa_gain: 0, pwr_idx: 19 },
-    { rf_power: 17, pa_gain: 1, pwr_idx: 1 },
-    { rf_power: 18, pa_gain: 1, pwr_idx: 2 },
-    { rf_power: 19, pa_gain: 1, pwr_idx: 3 },
-    { rf_power: 20, pa_gain: 1, pwr_idx: 4 },
-    { rf_power: 21, pa_gain: 1, pwr_idx: 5 },
-    { rf_power: 22, pa_gain: 1, pwr_idx: 6 },
-    { rf_power: 23, pa_gain: 1, pwr_idx: 7 },
-    { rf_power: 24, pa_gain: 1, pwr_idx: 8 },
-    { rf_power: 25, pa_gain: 1, pwr_idx: 10 },
-    { rf_power: 26, pa_gain: 1, pwr_idx: 13 },
-    { rf_power: 27, pa_gain: 1, pwr_idx: 16 }
-];
-const tx_gain_lut_868_high = [
-    { rf_power: 1, pa_gain: 0, pwr_idx: 4 },
-    { rf_power: 2, pa_gain: 0, pwr_idx: 5 },
-    { rf_power: 3, pa_gain: 0, pwr_idx: 6 },
-    { rf_power: 4, pa_gain: 0, pwr_idx: 7 },
-    { rf_power: 5, pa_gain: 0, pwr_idx: 8 },
-    { rf_power: 6, pa_gain: 0, pwr_idx: 9 },
-    { rf_power: 7, pa_gain: 0, pwr_idx: 10 },
-    { rf_power: 8, pa_gain: 0, pwr_idx: 11 },
-    { rf_power: 9, pa_gain: 0, pwr_idx: 12 },
-    { rf_power: 10, pa_gain: 0, pwr_idx: 13 },
-    { rf_power: 11, pa_gain: 0, pwr_idx: 14 },
-    { rf_power: 12, pa_gain: 0, pwr_idx: 15 },
-    { rf_power: 13, pa_gain: 0, pwr_idx: 16 },
-    { rf_power: 14, pa_gain: 0, pwr_idx: 17 },
-    { rf_power: 15, pa_gain: 0, pwr_idx: 18 },
-    { rf_power: 16, pa_gain: 0, pwr_idx: 19 },
-    { rf_power: 17, pa_gain: 1, pwr_idx: 1 },
-    { rf_power: 18, pa_gain: 1, pwr_idx: 2 },
-    { rf_power: 19, pa_gain: 1, pwr_idx: 3 },
-    { rf_power: 20, pa_gain: 1, pwr_idx: 4 },
-    { rf_power: 21, pa_gain: 1, pwr_idx: 5 },
-    { rf_power: 22, pa_gain: 1, pwr_idx: 6 },
-    { rf_power: 23, pa_gain: 1, pwr_idx: 7 },
-    { rf_power: 24, pa_gain: 1, pwr_idx: 8 },
-    { rf_power: 25, pa_gain: 1, pwr_idx: 10 },
-    { rf_power: 26, pa_gain: 1, pwr_idx: 13 },
-    { rf_power: 27, pa_gain: 1, pwr_idx: 16 }
-];
-
-const tx_gain_lut_470 = [
-    { rf_power: -6, pa_gain: 1, pwr_idx: 0 },
-    { rf_power: -3, pa_gain: 1, pwr_idx: 1 },
-    { rf_power: 0, pa_gain: 1, pwr_idx: 2 },
-    { rf_power: 3, pa_gain: 0, pwr_idx: 3 },
-    { rf_power: 6, pa_gain: 0, pwr_idx: 4 },
-    { rf_power: 10, pa_gain: 0, pwr_idx: 5 },
-    { rf_power: 11, pa_gain: 0, pwr_idx: 6 },
-    { rf_power: 12, pa_gain: 0, pwr_idx: 7 },
-    { rf_power: 13, pa_gain: 0, pwr_idx: 8 },
-    { rf_power: 14, pa_gain: 0, pwr_idx: 9 },
-    { rf_power: 16, pa_gain: 0, pwr_idx: 10 },
-    { rf_power: 20, pa_gain: 0, pwr_idx: 11 },
-    { rf_power: 23, pa_gain: 0, pwr_idx: 12 },
-    { rf_power: 25, pa_gain: 0, pwr_idx: 13 },
-    { rf_power: 26, pa_gain: 0, pwr_idx: 14 },
-    { rf_power: 27, pa_gain: 0, pwr_idx: 15 }
-];
-
 var freq_plan_table = {
     "US915": {
         "plans": [
@@ -183,14 +47,7 @@ var freq_plan_table = {
                 "mode": ["packet_forwarder"]
             }
         ],
-        "device": "US915",
-        "radio0": {
-            "rssi_offset": -213.0,
-            "tx_gain_lut": tx_gain_lut_915_high,
-        },
-        "radio1": {
-            "rssi_offset": -213.0,
-        }
+        "device": "US915"
     },
     "AU915": {
         "plans": [
@@ -235,14 +92,7 @@ var freq_plan_table = {
                 "mode": ["packet_forwarder"]
             }
         ],
-        "device": "US915",
-        "radio0": {
-            "rssi_offset": -213.0,
-            "tx_gain_lut": tx_gain_lut_915_low,
-        },
-        "radio1": {
-            "rssi_offset": -213.0,
-        }
+        "device": "US915"
     },
     "AS923": {
         "plans": [
@@ -277,14 +127,7 @@ var freq_plan_table = {
                 "mode": ["packet_forwarder"]
             }
         ],
-        "device": "US915",
-        "radio0": {
-            "rssi_offset": -213.0,
-            "tx_gain_lut": tx_gain_lut_915_high,
-        },
-        "radio1": {
-            "rssi_offset": -213.0,
-        }
+        "device": "US915"
     },
     "KR920": {
         "plans": [
@@ -294,14 +137,7 @@ var freq_plan_table = {
                 "mode": ["packet_forwarder"]
             }
         ],
-        "device": "US915",
-        "radio0": {
-            "rssi_offset": -213.0,
-            "tx_gain_lut": tx_gain_lut_915_high,
-        },
-        "radio1": {
-            "rssi_offset": -213.0,
-        }
+        "device": "US915"
     },
     "EU868": {
         "plans": [
@@ -311,14 +147,7 @@ var freq_plan_table = {
                 "mode": ["packet_forwarder"]
             }
         ],
-        "device": "EU868",
-        "radio0": {
-            "rssi_offset": -215.0,
-            "tx_gain_lut": tx_gain_lut_868_low,
-        },
-        "radio1": {
-            "rssi_offset": -215.0,
-        }
+        "device": "EU868"
     },
     "IN865": {
         "plans": [
@@ -328,14 +157,7 @@ var freq_plan_table = {
                 "mode": ["packet_forwarder"]
             }
         ],
-        "device": "EU868",
-        "radio0": {
-            "rssi_offset": -215.0,
-            "tx_gain_lut": tx_gain_lut_868_high,
-        },
-        "radio1": {
-            "rssi_offset": -215.0,
-        }
+        "device": "EU868"
     },
     "RU864": {
         "plans": [
@@ -345,14 +167,7 @@ var freq_plan_table = {
                 "mode": ["packet_forwarder"]
             }
         ],
-        "device": "EU868",
-        "radio0": {
-            "rssi_offset": -215.0,
-            "tx_gain_lut": tx_gain_lut_868_high,
-        },
-        "radio1": {
-            "rssi_offset": -215.0,
-        }
+        "device": "EU868"
     },
     "CN470": {
         "plans": [
@@ -417,66 +232,19 @@ var freq_plan_table = {
                 "mode": ["packet_forwarder"]
             }
         ],
-        "device": "CN470",
-        "radio0": {
-            "rssi_offset": -207.0,
-            "tx_gain_lut": tx_gain_lut_470,
-        },
-        "radio1": {
-            "rssi_offset": -207.0,
-        }
+        "device": "CN470"
     }
 }
 
 return view.extend({
-    setBasicStationRegion: function (region) {
-        var rfconfSections = uci.sections("basicstation", "rfconf");
-        var rfconf0 = rfconfSections.find(s => s['.name'] === 'rfconf0');
-        if (rfconf0) {
-            uci.set("basicstation", rfconf0['.name'], "rssiOffset", String(region.radio0.rssi_offset));
-        }
-        var rfconf1 = rfconfSections.find(s => s['.name'] === 'rfconf1');
-        if (rfconf1) {
-            uci.set("basicstation", rfconf1['.name'], "rssiOffset", String(region.radio1.rssi_offset));
-        }
-        // set txlut
-        var txlutSections = uci.sections("basicstation", "txlut");
-        var newLuts = region.radio0.tx_gain_lut;
-        for (var j = 0; j < newLuts.length; j++) {
-            var lut = newLuts[j];
-            var sectionName;
-
-            if (j < txlutSections.length) {
-                sectionName = txlutSections[j]['.name'];
-            } else {
-                sectionName = uci.add("basicstation", "txlut");
-            }
-
-            uci.set("basicstation", sectionName, "rfPower", lut.rf_power);
-            uci.set("basicstation", sectionName, "paGain", lut.pa_gain);
-            uci.set("basicstation", sectionName, "pwrIdx", lut.pwr_idx);
-            uci.set("basicstation", sectionName, "usedBy", "rfconf0");
-        }
-
-        // Avoid deleting sections while editing: it may trigger ubus "not found"
-        // when users change channel-plan multiple times before Save & Apply.
-        for (var k = newLuts.length; k < txlutSections.length; k++) {
-            var sid = txlutSections[k]['.name'];
-            uci.unset("basicstation", sid, "usedBy");
-            uci.unset("basicstation", sid, "rfPower");
-            uci.unset("basicstation", sid, "paGain");
-            uci.unset("basicstation", sid, "pwrIdx");
-        }
-    },
-
     setLoRaRegion: function (region) {
-        uci.set("packetforwarder", "sx130x", "region", region);
-        this.setBasicStationRegion(freq_plan_table[region]);
+        uci.set("packetforwarder", "@sx130x[0]", "region", region);
+        uci.set("basicstation", "@sx130x[0]", "region", region);
     },
 
     setLoRaChannelPlan: function (channelPlan) {
         uci.set("packetforwarder", "@sx130x[0]", "channel_plan", channelPlan);
-        // this.setBasicStationRegion(region);
+        // uci.set("basicstation", "@sx130x[0]", "channel_plan", channelPlan);
     },
 
     load: function () {
